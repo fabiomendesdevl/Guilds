eb3634ac392e3876e7524781c2ecbc449597f785
Fabio Mendes <fabiomendesdev@outlook.com>
2023-11-23 16:32:01 -0300

    Guilds - curso Balta

---
+++ Guilds/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Guilds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); 
+            var id = Guid.NewGuid();
+            Console.WriteLine(id);
+
+            id.ToString();
+            id = new Guid();
+            Console.WriteLine(id);
+        }
+    }
+}
